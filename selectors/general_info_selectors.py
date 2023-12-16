5466d02297fa38a4832e4b3a06f97f80cf931260
Stefan Petrovski <spetrovski09@gmail.com>
2023-12-16 18:56:51 +0100

    Refactor general info selectors

--- selectors/general_info_selectors.py
+++ selectors/general_info_selectors.py
@@ -1,13 +1,10 @@
-league_name_selector = '#fbheader > div:nth-child(2) > span.LName > span'
-date_time_selector = '#fbheader > div:nth-child(2) > span.time'
+league_name_selector = '#fbheader > div:nth-child(1) > span.LName > span'
+date_time_selector = '#fbheader > div:nth-child(1) span.time'
 
-first_team_name_selector = '#fbheader > div:nth-child(3) > div.home > div'
-second_team_name_selector = '#fbheader > div:nth-child(3) > div.guest > div'
+first_team_name_selector = '#fbheader > div:nth-child(2) > div.home > div.sclassName'
+second_team_name_selector = '#fbheader > div:nth-child(2) > div.guest > div.sclassName'
 
 first_team_goals_scored_selector = '#mScore > div > div:nth-child(1)'
 second_team_goals_scored_selector = '#mScore > div > div:nth-child(3)'
 
-game_state_selector = '#mScore > div > div:nth-child(2) > div'
-
-first_half_score_selector = '#mScore > div > div:nth-child(2) > span > span:nth-child(1)'
-second_half_score_selector = '#mScore > div > div:nth-child(2) > span > span:nth-child(2)'
+game_state_selector = '#mScore > div > div:nth-child(2) > div.state'
