0357ee76a725f2686b95c7ac1e10eec6907488e2
Stefan Petrovski <spetrovski09@gmail.com>
2023-12-17 02:19:00 +0100

    Refactor next game info selectors

---
+++ selectors/next_game_days_until_location_selectors.py
@@ -0,0 +1,5 @@
+first_team_days_until_next_game_selector = '#dv_futer > div > div.home-div > ul > li:nth-child(1) > div.corteam > div.interval'
+second_team_days_until_next_game_selector = '#dv_futer > div > div.guest-div > ul > li:nth-child(1) > div.corteam > div.interval'
+
+is_first_team_next_game_at_home_selector = '#dv_futer > div > div.home-div > ul > li:nth-child(1) > div.corteam > div.team > span:nth-child(1)'
+is_second_team_next_game_at_home_selector = '#dv_futer > div > div.guest-div > ul > li:nth-child(1) > div.corteam > div.team > span:nth-child(1)'
