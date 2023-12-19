36cc93a559a588a8de1fb9fa5bcb08b76812e4e0
Stefan Petrovski <spetrovski09@gmail.com>
2023-12-19 02:41:40 +0100

    Add last game info selectors

---
+++ selectors/last_game_days_since_location_selectors.py
@@ -0,0 +1,5 @@
+first_team_last_game_date_selector = '#tb_home_recent > li:nth-child(1) > div.corteam > div.time > span'
+second_team_last_game_date_selector = '#tb_guest_recent > li:nth-child(1) > div.corteam > div.time > span'
+
+was_first_team_last_game_at_home_selector = '#tb_home_recent > li:nth-child(1) > div.corteam > div.team > span:nth-child(1)'
+was_second_team_last_game_at_home_selector = '#tb_guest_recent > li:nth-child(1) > div.corteam > div.team > span:nth-child(1)'
