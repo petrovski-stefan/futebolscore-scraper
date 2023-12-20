fab051c1c1d83161b1276d91f931ecff5d8cc540
Stefan Petrovski <spetrovski09@gmail.com>
2023-12-20 01:09:11 +0100

    Update current form selectors

---
+++ selectors/current_form_averages_selectors.py
@@ -0,0 +1,8 @@
+first_team_scoring_average_selector = '#dv_recent_stats .bar:nth-of-type(1) div.ext.win-f'
+second_team_scoring_average_selector = '#dv_recent_stats .bar:nth-of-type(1) div.ext.p2'
+
+first_team_conceding_average_selector = '#dv_recent_stats .bar:nth-of-type(2) div.ext.win-f'
+second_team_conceding_average_selector = '#dv_recent_stats .bar:nth-of-type(2) div.ext.p2'
+
+same_league_button_selector = '#dv_recent > div.match-odds-menu > div > span:nth-child(1)'
+correct_home_away_button_selector = '#dv_recent > div.match-odds-menu > div > span:nth-child(2)'
