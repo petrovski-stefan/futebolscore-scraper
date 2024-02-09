d4a20c5acd9822f999516f7992ae988a9ad3cfd4
Stefan Petrovski <spetrovski09@gmail.com>
2024-02-09 01:52:49 +0100

    Remove useless driver logs

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def config_driver():
     chrome_options.add_experimental_option(
         "excludeSwitches", ["enable-logging"])
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('log-level=3')
     driver = webdriver.Chrome(options=chrome_options)
     driver.set_page_load_timeout(30)
 
